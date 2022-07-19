89720a60fafe85716812f333570aabbc6530d8ae
Daniel de Paula <daniel.c.paula@hotmail.com>
2022-07-19 17:37:40 -0300

    estudo

--- rocketseat/explorer/aulas/nivel10/rocketnotes/src/pages/Home/index.jsx
+++ rocketseat/explorer/aulas/nivel10/rocketnotes/src/pages/Home/index.jsx
@@ -1,5 +1,8 @@
-import { FiPlus, FiSearch } from 'react-icons/fi'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { FiPlus, FiSearch } from 'react-icons/fi'
+
+import { api } from '../../services/api'
 
 import {Container, Brand, Menu, Search, Content, NewNote} from './styles'
 import { Header } from '../../components/Header'
@@ -9,6 +12,47 @@ import { Note } from '../../components/Note'
 import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
+  const [search, setSearch] = useState("")
+  const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+  const [notes, setNotes] = useState([])
+
+  function handleTagSelected(tagName) {
+    if (tagName === "all") {
+      return setTagsSelected([])
+    }
+
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    if(alreadySelected) {
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName)
+      setTagsSelected(filteredTags)
+    } else {
+      setTagsSelected(prevState => [...prevState, tagName])
+    }
+    
+  }
+
+  function handleDetails(id) {
+
+  }
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags")
+      setTags(response.data)
+    }
+    fetchTags()
+  },[])
+
+  useEffect(() => {
+    async function fetchNotes() {
+      const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`)
+      setNotes(response.data)
+    }
+    fetchNotes()
+  }, [tagsSelected, search])
+
   return (
     <Container>
       <Brand>
@@ -18,27 +62,43 @@ export function Home() {
       <Header />
 
       <Menu>
-        <li><ButtonText title="Todos" isActive/></li>
-        <li><ButtonText title="React"/></li>
-        <li><ButtonText title="Node"/></li>
-    
+        <li>
+          <ButtonText 
+            title="Todos" 
+            onClick ={() => handleTagSelected("all")}
+            isActive={tagsSelected.length === 0}
+          />
+        </li>
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+              <ButtonText 
+                title={tag.name}
+                onClick ={() => handleTagSelected(tag.name)}
+                isActive={tagsSelected.includes(tag.name)}
+              />
+            </li>
+          ))
+        }
       </Menu>
 
       <Search>
-        <Input  placeholder="Pesquisar pelo título" icon={FiSearch}/>
+        <Input  
+          placeholder="Pesquisar pelo título"
+          onChange={(e) => setSearch(e.target.value)} 
+          icon={FiSearch}/>
       </Search>
         
       <Content>
         <Section title="Minhas Notas">
-          <Note data=
-            {{
-              title: 'React', 
-              tags: [
-                  {id: '1', name:'react'},
-                  {id: '2', name: 'nodejs'}
-                ]
-              }} 
+          {
+            notes.map(note => (
+              <Note
+                key={String(note.id)}
+                data={note}
             />
+            ))
+          }
         </Section>
       </Content>
 
